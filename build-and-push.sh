b7f3a61405d5dc884d6d8174d85764ec555f6803
Prabhat Sharma <hi.prabhat@gmail.com>
2021-10-29 11:54:21 -0700

    added local build script

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+VERSION=v4
+docker build . -t ab3-order:$VERSION -t ab3-order:latest 
+
+docker tag ab3-order:latest 525158249545.dkr.ecr.us-west-2.amazonaws.com/ab3-order:latest
+docker tag ab3-order:latest 525158249545.dkr.ecr.us-west-2.amazonaws.com/ab3-order:$VERSION 
+
+aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 525158249545.dkr.ecr.us-west-2.amazonaws.com
+
+docker push 525158249545.dkr.ecr.us-west-2.amazonaws.com/ab3-order:latest
+docker push 525158249545.dkr.ecr.us-west-2.amazonaws.com/ab3-order:$VERSION 
+
+
--- start-test.sh
+++ start-test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-hey -n 2 -c 1 -q 1 -m POST -T application/json -D testdata.json http://localhost:8082/order
+hey -n 100 -c 1 -q 1 -m POST -T application/json -D testdata.json http://localhost:8082/order
